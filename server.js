0c98c22f999e953e355fdf9f6ad445083f303ae4
lucashenriquelhlh44-code <lucashenrique.lh.lh44@gmail.com>
2026-08-27 14:10:09 -0300

    Update server.js

--- server.js
+++ server.js
@@ -241,7 +241,6 @@ app.get('/api/ordens', (req, res) => {
 
 
 const PORT = process.env.PORT || 3000;
-
-
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
-
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
